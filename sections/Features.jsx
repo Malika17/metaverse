2b256a270c6c3a82517d3903bcf536adcb6511b1
Malika17 <malikasharma1711@gmail.com>
2025-05-10 00:53:40 +0530

    ffmpeg edited

--- sections/Features.jsx
+++ sections/Features.jsx
@@ -5,27 +5,27 @@ import { EmblaCarousel } from "../components";
 const Features = () => {
   const slides = [
     {
-      videoSrc: "/videos/f1.mp4",
+      videoSrc: "/videos/f1new.mp4",
       leftText: "Personalized feed based on your interests.",
       rightText: "Discover content from brands and creators you like.",
     },
     {
-      videoSrc: "/videos/f2.mp4",
+      videoSrc: "/videos/f2new.mp4",
       leftText: "Tap posts to view and buy products directly.",
       rightText: "Shop seamlessly from the content you engage with.",
     },
     {
-      videoSrc: "/videos/f3.mp4",
+      videoSrc: "/videos/f3new.mp4",
       leftText: "Learn the story behind each brand.",
       rightText: "Follow creators and explore their products.",
     },
     {
-      videoSrc: "/videos/f4.mp4",
+      videoSrc: "/videos/f4new.mp4",
       leftText: "Search products, posts, and brands easily.",
       rightText: "Find what you're looking for with personalized searches.",
     },
     {
-      videoSrc: "/videos/f5.mp4",
+      videoSrc: "/videos/f5new.mp4",
       leftText: "Add products to your cart directly from posts.",
       rightText: "Enjoy a fast and secure checkout experience.",
     },
