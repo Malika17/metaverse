25379f6199eb84399f491db92bbaaf0ecc521f1f
Malika17 <malikasharma1711@gmail.com>
2025-05-10 20:20:34 +0530

    cta added

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,15 +1,14 @@
-// lib/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhHpvdlK5VnENPdcdBgBRa9XtR6qqPCNY",
-  authDomain: "loresellerreg.firebaseapp.com",
-  projectId: "loresellerreg",
-  storageBucket: "loresellerreg.firebasestorage.app",
-  messagingSenderId: "807337521757",
-  appId: "1:807337521757:web:b5dae95238854bc3f393b4",
-  measurementId: "G-KBLMW54VWT",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
